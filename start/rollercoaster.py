840d48a3144b735dfcde65637af2c2e3793762cc
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-04 20:23:31 +0330

    make new directory and mage new py file, the name is rollercoaster

---
+++ start/rollercoaster.py
@@ -0,0 +1,15 @@
+# make rollercoaster and filter the age
+
+print("welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+    print("Your can ride the rollercoaster!")
+    age = int(input("Please Enter your age in years? "))
+    if age <= 18:
+        print("You should pay $7.")
+    elif 45 <= age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
+    else:
+        print("You should pay $12.")
+else:
+    print("Your can't ride the rollercoaster!")
