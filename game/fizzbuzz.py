269583ae51ca5e49690e23be224816abf6930f94
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-09 22:35:20 +0330

    change fizzbuzz file

--- game/fizzbuzz.py
+++ game/fizzbuzz.py
@@ -1,10 +1,10 @@
-# make fizzbuzz with python
+# make fizzbuzz with python for your kid
 for fizzbuzz in range(1, 101):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0: # check 3 & 5 to 0
         print("FizzBuzz")
-    elif fizzbuzz % 3 == 0:
+    elif fizzbuzz % 3 == 0: # check 3 to 0
         print("Fizz")
-    elif fizzbuzz % 5 == 0:
+    elif fizzbuzz % 5 == 0: # check 5 to 0
         print("buzz")
     else:
         print(fizzbuzz)
