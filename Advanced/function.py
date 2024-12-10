29fb41d6f3a2c20d074f5b4c5b8040933063daa8
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-10 21:37:30 +0330

    make new file with name is function

---
+++ Advanced/function.py
@@ -0,0 +1,14 @@
+def my_function(a, b):
+    c = a + b - a ** 2
+    return c
+test = my_function(1, 3)
+print(f"{test} is answer of def\n")
+
+# While loop
+
+user = 6
+
+while user > 0:
+    print(f"{user} is answer of while loop")
+    user -= 1
+print(f"{user} is less than 6")
