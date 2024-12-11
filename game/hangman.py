6d200964ed39e2397d34dfb5d4a10fa3e08c0ce2
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-11 22:29:53 +0330

    change hangman game V1

--- game/hangman.py
+++ game/hangman.py
@@ -1,10 +1,11 @@
-# comming soon
-print(""" 
-        _______
-     |/      | 
-     |      (_)
-     |      \|/
-     |       |
-     |      / /
-     |
-  ___|___  """)
+# version 1 of hangman game
+
+# import random library
+import random
+
+# make list by name word_list
+word_list = ["aardvark", "baboon", "camel"]
+
+# choose word in word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
