677ce1947f0b3011f08c35e9e2d31508352f4807
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-17 23:54:56 +0330

    add comment for loop file

--- start/for.py
+++ start/for.py
@@ -1,9 +1,10 @@
 # for loop
-# for i in range(1, 100):
-#     print(i)
+for i in range(1, 100): # in this section print 1 to 99
+    print(i)
 
 # for with input
 receive_user = int(input("add number: "))
 
 for i in range(0, receive_user):
-    print(i)
+    print(i)  # in this way we print one by one item
+
