7a0eb5539532864b3c85858461fcbcb90ec01032
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-17 23:53:36 +0330

    Work with function and input

--- start/for.py
+++ start/for.py
@@ -1,3 +1,9 @@
 # for loop
-for i in range(1, 100):
+# for i in range(1, 100):
+#     print(i)
+
+# for with input
+receive_user = int(input("add number: "))
+
+for i in range(0, receive_user):
     print(i)
