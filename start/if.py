fdee6fb203ea45ebd495eab10ca71574ef3a7690
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-18 23:09:26 +0330

    first line in if.py receive user

--- start/if.py
+++ start/if.py
@@ -1,2 +1,3 @@
 # work with if 
 # Create new program, user give number with int type and after that check this number is even or odd ?
+receive_user = input("Please enter number and I check your number is odd or even?")
