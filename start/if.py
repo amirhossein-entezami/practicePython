474a8cf3bf382c4491412274fea19e326d831e91
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-19 23:51:36 +0330

    save

--- start/if.py
+++ start/if.py
@@ -2,6 +2,6 @@
 # Create new program, user give number with int type and after that check this number is even or odd ?
 receive_user = input("Please enter number and I check your number is odd or even?")
 if receive_user % 2 == 0:
-    print("your number is even!") # even number
+    print("your number is even!") # Even Number
 else:
-    print("your number is odd!") # odd number
+    print("your number is odd!") # Odd Number
