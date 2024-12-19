abe85019264d73cf42abf7d24eda58eb1e63410f
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-19 23:53:31 +0330

    save

--- start/print.py
+++ start/print.py
@@ -1,4 +1,4 @@
 # print() function, at the first we talk about print function
 print('Hello AmiR!!!')
-print("Hello World!\nHello World!")
-print("Hello" + "AmiR" + "programer!")
+print("Hello World!\nHello Amir!")
+print("Hello" + "AmiR is" + "programer!")
