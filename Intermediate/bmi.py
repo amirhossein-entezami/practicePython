5fec2c6c80b121e272e95d2ed00966b8ff17f20b
Amirhossein Entezami | امیرحسین انتظامی <83422700+amirhossein-entezami@users.noreply.github.com>
2024-12-23 23:46:10 +0330

    Update bmi.py

--- Intermediate/bmi.py
+++ Intermediate/bmi.py
@@ -2,7 +2,7 @@
 receive_weight = int(input("Please import your weight in kilograms: "))
 receive_height = float(input("Please import your height in centimeter: "))
 
-# out put should be round number -> we use int() function
+# out put should be round number, we use int() function
 bmi = int(receive_weight / (receive_height ** 2))
 
-print(f"Your bmi is {bmi}")
+print(f"Your bmi is {bmi}") #show your bmi
