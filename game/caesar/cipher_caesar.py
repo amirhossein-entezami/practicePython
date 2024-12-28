a05707a6985782a69d31312e132f05490c268873
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-28 23:35:05 +0330

    give user information in cipher_caesar

--- game/caesar/cipher_caesar.py
+++ game/caesar/cipher_caesar.py
@@ -1,6 +1,7 @@
 from art_caesar import logo
 print(logo)
 
+# alphabet for encode and decode letter user
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f'
     , 'g', 'h', 'i', 'j', 'k', 'l'
     , 'd', 'm', 'n', 'o', 'p', 'q'
@@ -11,3 +12,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f'
     , 'o', 'p', 'q', 'r', 's', 't'
     , 'u', 'v', 'w', 'x', 'y', 'z']
 
+# Give input user for start the program
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+text = input("Type your message: \n").lower()
+shift = int(input("Type the shift number: \n"))
