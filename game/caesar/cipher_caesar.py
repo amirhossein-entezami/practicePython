95df7bc387e4670fa40528ceacf4328ce082cf0c
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-28 23:30:56 +0330

    start make new program, cipher ceaser

---
+++ game/caesar/cipher_caesar.py
@@ -0,0 +1,13 @@
+from art_caesar import logo
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f'
+    , 'g', 'h', 'i', 'j', 'k', 'l'
+    , 'd', 'm', 'n', 'o', 'p', 'q'
+    , 'r', 's', 't', 'u', 'v', 'w'
+    , 'x', 'y', 'z', 'a', 'b', 'c'
+    , 'd', 'e', 'f', 'g', 'h', 'i'
+    , 'j', 'k', 'l', 'd', 'm', 'n'
+    , 'o', 'p', 'q', 'r', 's', 't'
+    , 'u', 'v', 'w', 'x', 'y', 'z']
+
