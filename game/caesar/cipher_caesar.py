08f07fe9b21da066c7d504d7fef801d0cf92372f
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-29 23:50:36 +0330

    compelte the def in cipher_ceaser.py

--- game/caesar/cipher_caesar.py
+++ game/caesar/cipher_caesar.py
@@ -14,7 +14,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f'
 
 
 def encrypt(plain_text, shift_amount):
-    pass
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is: {cipher_text}")
 
 # Give input user for start the program
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
