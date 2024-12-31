dd17cac9c66ae84f93a963685e5e63533ab07781
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-31 23:57:14 +0330

    make new dict

--- start/dictionary.py
+++ start/dictionary.py
@@ -1,1 +1,9 @@
 # This file about dictionary 
+
+# make a new dict
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can that you can easily call over and over again."
+}
+
