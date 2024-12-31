a6ed75a6e76bbd81be7bcd5954fb822c5ac8e824
Amirhosein Entezami <am.entezami1321@gmail.com>
2024-12-31 23:58:15 +0330

    work on dict

--- start/dictionary.py
+++ start/dictionary.py
@@ -7,3 +7,12 @@ programming_dictionary = {
     "Function": "A piece of code that you can that you can easily call over and over again."
 }
 
+# Retrieving items from dictionary.
+# print(programming_dictionary["Function"])
+
+# Adding new items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+# Create an empty dictionary
+empty_dictionary = {}
+
