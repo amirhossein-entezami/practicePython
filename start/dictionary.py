c246e4983a619f9e91846f365ca75747d32105d6
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-01 23:26:07 +0330

    nesting

--- start/dictionary.py
+++ start/dictionary.py
@@ -24,3 +24,25 @@ empty_dictionary = {}
 programming_dictionary["Bug"] = "A moth in your computer."
 # print(programming_dictionary)
 
+# Nesting
+
+travel_log_dict = {
+    "France": {"visited_city": ["paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"visited_city": ["Berlin", "Hamburg"], "total_visits": 5},
+}
+
+# Nesting Dictionary in a List
+
+travel_log_list = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    },
+]
+
