cd624e7837f7e88dbf4f8090026f67336f3fcb29
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-02 23:35:29 +0330

    cheange new_method_in_dict.py

--- start/dictionary/new_method_in_dict.py
+++ start/dictionary/new_method_in_dict.py
@@ -13,3 +13,11 @@ travel_log = [
     }
 ]
 
+# TODO - Create a new function to add new country to travel_log
+
+def add_new_country(country, city_visited, total_visited):
+    travel_log.append({"country": country, "city_visited": city_visited, "total_visited":total_visited})
+
+
+add_new_country(country="Russia", total_visited=2, city_visited=["Moscow", "Saint Petersburg"])
+print(travel_log)
