6523e1c35551f165faba1693972b76d9a3970e44
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-02 23:31:35 +0330

    make new list with dict inside by name travel_log

--- start/dictionary/new_method_in_dict.py
+++ start/dictionary/new_method_in_dict.py
@@ -1,1 +1,15 @@
 # we work with dictionary and we append new item in dict and list
+
+travel_log = [
+    {
+        "country": "France",
+        "city_visited": ["Paris", "Little", "Dijon"],
+        "total_visited": 12,
+    },
+    {
+        "country": "Germany",
+        "city_visited": ["Hamburg", "Berlin", "stuttgart"],
+        "total_visited": 5
+    }
+]
+
