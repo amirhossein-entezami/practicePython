fd037041f560a8fb89beaa1b59f325fa9a2b8582
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-04 23:43:36 +0330

    check calculator.py

--- Advanced/calculator.py
+++ Advanced/calculator.py
@@ -1,16 +1,16 @@
 # make a calculator
 
 # This is my practice
-# # Calculator
-# def calculator(num_1, num_2, operator):
-#     if operator == '+':
-#         return num_1 + num_2
-#
-#     elif operator == '-':
-#         return num_2 - num_1
-#
-#     elif operator == '*':
-#         return num_1 * num_2
-#
-#     elif operator == '/':
-#         return num_1 / num_2
+# Calculator
+def calculator(num_1, num_2, operator):
+    if operator == '+':
+        return num_1 + num_2
+
+    elif operator == '-':
+        return num_2 - num_1
+
+    elif operator == '*':
+        return num_1 * num_2
+
+    elif operator == '/':
+        return num_1 / num_2
