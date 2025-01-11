4cdc95b2aaa4565bd399b9a46c79f888300f3cec
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-11 18:36:06 +0330

    clear balckjack file

--- game/Difficult/balckjack.py
+++ game/Difficult/balckjack.py
@@ -1,16 +1,1 @@
-# make new game with balckjack
-
-cards = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10}
-
-receive_user = int(input("receive user: "))
-def user():
-    pass
-
-def computer():
-    pass
-
-
-if receive_user < 17:
-    pass
-elif receive_user > 21:
-    pass
+# clear the file and rewrite 
