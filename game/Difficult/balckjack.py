c8189404a30fd4a486d16426166305465da49289
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-11 18:45:41 +0330

    create function by name deal_card() in blackjack.py

--- game/Difficult/balckjack.py
+++ game/Difficult/balckjack.py
@@ -3,3 +3,10 @@
 import random
 import clear
 
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
