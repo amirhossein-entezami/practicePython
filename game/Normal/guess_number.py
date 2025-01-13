984c699ea88f47060aac23f9dd39ba208791457b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-13 23:52:17 +0330

    add while and def in guess_number.py

--- game/Normal/guess_number.py
+++ game/Normal/guess_number.py
@@ -8,4 +8,24 @@ for i in range(101):
 
 random_number = random.choice(numbers)
 
+# should_continue = True
+# while should_continue:
+#     def mode(easy, hard, difficulty):
+#         def guess(user_number, computer_number):
+#             global should_continue
+#             if user_number > computer_number:
+#                 print("Your number is too high")
+#             elif user_number < computer_number:
+#                 print("Your number is too low")
+#             elif user_number == computer_number:
+#                 print(f"Correct, computer number is {computer_number}, Your number is {user_number}")
+#                 should_continue = False
+#             else:
+#                 print("wrong")
+#
+#         guess(user_number=receive_user, computer_number=random_number)
+#
+#
+#     receive_user = int(input("please enter number: "))
+#     receive_mode = input("please enter mode, easy - hard - difficulty: ")
 
