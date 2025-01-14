4241e10ddd8bde1596d4aad4e6a63e535c00eaa6
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-14 23:36:59 +0330

    work with dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -13,3 +13,14 @@ def my_function():
         if i == 20:
             print("You got it!")
 my_function()
+
+
+######################################
+
+# Reproduce the Bug
+# False -------------------
+from random import randint
+dice_imgs = ["test_1","test_2","test_3","test_4","test_5","test_6",]
+dice_num = randint(1,6)
+print(dice_imgs[dice_num])
+
