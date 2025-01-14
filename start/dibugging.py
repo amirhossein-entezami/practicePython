c36b7acaac84564cec5ca93226fb907b8c4118df
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-14 23:37:36 +0330

    fix dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -24,3 +24,9 @@ dice_imgs = ["test_1","test_2","test_3","test_4","test_5","test_6",]
 dice_num = randint(1,6)
 print(dice_imgs[dice_num])
 
+# True----------------------
+from random import randint
+dice_imgs = ["test_1","test_2","test_3","test_4","test_5","test_6",]
+dice_num = randint(0,5)
+print(dice_imgs[dice_num])
+
