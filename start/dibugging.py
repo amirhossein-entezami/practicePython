50bd66eaea83ab250fa68b84188b1e4777192771
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-14 23:35:29 +0330

    work with dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -1,1 +1,8 @@
 # make dibugging check file
+# Describe Problem
+# False -----------
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+my_function()
