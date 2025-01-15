793b5e70117ba3fafc6e5e0b359c9176e5a631ba
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-15 16:15:46 +0330

    fixing new year dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -38,3 +38,13 @@ if year > 1980 and year < 1994:
     print("You are a millenial.")
 elif year > 1994:
     print("You are a Gen Z.")
+
+# True----------------------------
+year = int(input("What's your year of birth? "))
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+elif year >= 1994:
+    print("You are a Gen Z.")
+
+########################################
+
