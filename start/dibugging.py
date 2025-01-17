b7959e299e7e5fab70532a09fe8ff4ed700f7a8e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-17 23:33:24 +0330

    fix pages dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -64,9 +64,19 @@ if age > 18:
 
 # Print is Your Friend
 # False------------------------------
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words == pages * word_per_page
+# print(total_words)
+
+# True-------------------------------
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
 word_per_page = int(input("Number of words per page: "))
-total_words == pages * word_per_page
+total_words = pages * word_per_page
 print(total_words)
+
+#########################################################
