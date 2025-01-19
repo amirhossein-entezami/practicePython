c70ed45120119a0f6520411528962febb5f33034
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-19 23:37:41 +0330

    hide and comment more practice in dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -106,11 +106,11 @@ print(total_words)
 ############################################
 # More practice
 # False-------------
-number = int(input("Which number do you want to check? "))
+# number = int(input("Which number do you want to check? "))
 
-if number % 2 = 0:
-    print("The number is an even number.")
-else:
-    print("The number is an odd number.")
+# if number % 2 = 0:
+#     print("The number is an even number.")
+# else:
+#     print("The number is an odd number.")
 
 
