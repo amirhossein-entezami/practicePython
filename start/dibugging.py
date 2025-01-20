e4f03a62bd7651d24baf5d71d43ded852ce038d2
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-20 23:46:54 +0330

    comment year leap in dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -139,16 +139,16 @@ print(total_words)
     # print("Not a leap year.")
 
 # True----------
-year = int(input("Which year do you want to check? "))
+# year = int(input("Which year do you want to check? "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("is a leap year.")
-        else:
-            print("is not a leap year.")
-    else:
-        print("Leap Year.")
-else:
-    print("Not a leap year.")
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("is a leap year.")
+#         else:
+#             print("is not a leap year.")
+#     else:
+#         print("Leap Year.")
+# else:
+#     print("Not a leap year.")
 
