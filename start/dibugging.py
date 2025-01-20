87ce2f72fdcdf09204c4f8adbaf89c1fb1b78595
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-20 23:48:47 +0330

    fix fizzbuzz in dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -165,3 +165,15 @@ print(total_words)
 #         print("Buzz")
 #     else:
 #         print([number])
+
+# True----------------
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+        
