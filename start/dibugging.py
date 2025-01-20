c72cd75df8074909753865fa459bd2c5c4b5cd00
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-20 23:49:13 +0330

    fizbuzz comment in dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -167,13 +167,13 @@ print(total_words)
 #         print([number])
 
 # True----------------
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
         
