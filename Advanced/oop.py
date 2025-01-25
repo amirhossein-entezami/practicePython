b55b7c050796d8bf5d5a0be7ea68456836f66575
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-01-25 19:54:12 +0330

    pretty table import and install and use with add_column oop.py

--- Advanced/oop.py
+++ Advanced/oop.py
@@ -22,3 +22,10 @@ timmy.color("coral")
 timmy.forward(100)
 
 my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Chamander", "Pangoro"])
+table.add_column("Type", ["Electric", "Water", "water"])
+
+print(table)
