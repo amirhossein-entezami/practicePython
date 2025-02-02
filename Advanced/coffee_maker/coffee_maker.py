e6fe4064a3343e61e528bd97c7fefd9924450870
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-02 21:57:35 +0330

    complete the coffe_maker.py in coffe_maker

--- Advanced/coffee_maker/coffee_maker.py
+++ Advanced/coffee_maker/coffee_maker.py
@@ -6,13 +6,13 @@ class CoffeeMaker:
             "milk": 200,
             "coffee": 100,
         }
-        
+
     def report(self):
-        """Prints a report of all resources"""
+        """Prints a report of all resources."""
         print(f"Water: {self.resources['water']}ml")
-        print(f"Water: {self.resources['milk']}ml")
-        print(f"Water: {self.resources['coffee']}g")
-        
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
+
     def is_resource_sufficient(self, drink):
         """Returns True when order can be made, False if ingredients are insufficient."""
         can_make = True
@@ -21,12 +21,9 @@ class CoffeeMaker:
                 print(f"Sorry there is not enough {item}.")
                 can_make = False
         return can_make
-    
+
     def make_coffee(self, order):
         """Deducts the required ingredients from the resources."""
         for item in order.ingredients:
             self.resources[item] -= order.ingredients[item]
         print(f"Here is your {order.name} ☕️. Enjoy!")
-        
-
-    
