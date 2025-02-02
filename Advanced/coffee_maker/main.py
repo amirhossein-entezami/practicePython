2b420262ae85693ea558b01312c61455340f59c2
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-02 21:56:51 +0330

    complete the main.py in coffe_maker

--- Advanced/coffee_maker/main.py
+++ Advanced/coffee_maker/main.py
@@ -1,4 +1,3 @@
-# main.py for coffee maker
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -12,4 +11,15 @@ is_on = True
 coffee_maker.report()
 money_machine.report()
 
-
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
