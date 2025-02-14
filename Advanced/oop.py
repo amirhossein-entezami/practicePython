7b4c5b59722374c8a1d1a8dbec54cc0bc004bc89
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-14 23:10:29 +0330

    change and check oop.py

--- Advanced/oop.py
+++ Advanced/oop.py
@@ -23,9 +23,9 @@ timmy.forward(100)
 
 my_screen.exitonclick()
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Chamander", "Pangoro"])
-table.add_column("Type", ["Electric", "Water", "water"])
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Chamander", "Pangoro"])
+# table.add_column("Type", ["Electric", "Water", "water"])
 
 # print(table)
