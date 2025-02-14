e6be59f7823bddf693391a5e9c86940a2f1064e7
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-14 23:14:38 +0330

    change and check main.py

--- Advanced/coffee_maker/main.py
+++ Advanced/coffee_maker/main.py
@@ -1,10 +1,10 @@
-from menu import Menu, MenuItem
+# from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
-menu = Menu()
+# menu = Menu()
 
 is_on = True
 
