554207c0fe1fac82de2b9c93f223fd5434cd5173
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-20 23:33:45 +0330

    square shape with short way in turtle main.py

--- GUI/Turtle/main.py
+++ GUI/Turtle/main.py
@@ -12,13 +12,18 @@ timmy.shape("turtle")
 timmy.color("red")
 
 # Square Shape -> long way
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
-timmy.right(90)
-timmy.forward(100)
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+# timmy.right(90)
+# timmy.forward(100)
+
+# Square Shape -> short way
+for _ in range(4):
+    timmy.forward(100)
+    timmy.right(90)
 
 # end of the project
 screen = Screen()
