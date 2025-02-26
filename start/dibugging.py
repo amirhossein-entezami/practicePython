8aeff16c59eb41039115ae44f641032c5a0bad00
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-26 23:42:52 +0330

    fix problem with dibugging.py

--- start/dibugging.py
+++ start/dibugging.py
@@ -1,3 +1,4 @@
+from random import randint
 # make dibugging check file
 # Describe Problem
 # False -----------
@@ -19,13 +20,11 @@ my_function()
 
 # Reproduce the Bug
 # False -------------------
-from random import randint
 dice_imgs = ["test_1","test_2","test_3","test_4","test_5","test_6",]
 dice_num = randint(1,6)
 print(dice_imgs[dice_num])
 
 # True----------------------
-from random import randint
 dice_imgs = ["test_1","test_2","test_3","test_4","test_5","test_6",]
 dice_num = randint(0,5)
 print(dice_imgs[dice_num])
