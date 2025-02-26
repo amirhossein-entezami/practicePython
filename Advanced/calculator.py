2d3cb2308502efdb5ab5a1ca5bc314c4eb29eddc
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-02-26 23:46:05 +0330

    fix calculatior bug

--- Advanced/calculator.py
+++ Advanced/calculator.py
@@ -4,15 +4,14 @@
 def calculator(num_1, num_2, operator):
     if operator == '+':
         return num_1 + num_2
-
     elif operator == '-':
         return num_2 - num_1
-
     elif operator == '*':
         return num_1 * num_2
-
     elif operator == '/':
         return num_1 / num_2
+    else:
+        print("this is wrong type!!!")
 
 receive_num_1 = int(input("What's the first number? "))
 print("""
