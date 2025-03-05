9056aa4d3085524b726db753c1380896481aa2da
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-05 23:31:47 +0330

    add new code on offer.py in projcet

--- Project/offer.py
+++ Project/offer.py
@@ -1,2 +1,6 @@
 """ barnamei ke az vorodi mablagh daryaft
 kone ba ehtesab 20 % maliat mide """
+
+print("Gheymat mashi morede nazar bede malyat begam!")
+receive_user = int(input("mablagh morede nazare khodeto bezan: "))
+count_darsad = int(receive_user * 0.2)
