c226d09104a25e727a3a582e41ff5054b7bedcfb
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-05 23:34:54 +0330

    add new line on sqrt.py in project

--- Project/sqrt.py
+++ Project/sqrt.py
@@ -1,1 +1,4 @@
 """ barnamei ke yek add daryaft mikone jazresh ro namayesh mide """
+
+receive_user = int(input("Add bedid jazr add bedam: "))
+print(f"addi ke dadid jazresh mishe -> {int(receive_user ** .5)}")
