67b8657d8668b9baa205309b0eb81c8f9ed330ac
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-05 23:32:30 +0330

    add new line code and give output on offer.py in projcet

--- Project/offer.py
+++ Project/offer.py
@@ -4,3 +4,5 @@ kone ba ehtesab 20 % maliat mide """
 print("Gheymat mashi morede nazar bede malyat begam!")
 receive_user = int(input("mablagh morede nazare khodeto bezan: "))
 count_darsad = int(receive_user * 0.2)
+print(f"Mliat shoma mishe -> ( {count_darsad} ) toman!")
+print(f"Enghdar hesab malyat mashinet mishe -> ( {receive_user + count_darsad} ) toman!")
