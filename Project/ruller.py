854d28afa80c46a822888d2f8b33195ba9398f09
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-05 23:37:28 +0330

    add new line and give number from user on ruller.py in project

--- Project/ruller.py
+++ Project/ruller.py
@@ -1,2 +1,4 @@
 """barname ke milimter dryaft mikone va 
 mige chand santi meter va chand meter hast"""
+
+receive_num = int(input("add bede begam chand centimetere ? va chand meter?(!!faghat 4 ragham!!): "))
