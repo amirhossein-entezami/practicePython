ad612f2fb650c3b4c369880b17ad58c0b2ecf58d
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-05 23:38:30 +0330

    add new line and make conditional for user on ruller.py in project

--- Project/ruller.py
+++ Project/ruller.py
@@ -2,3 +2,12 @@
 mige chand santi meter va chand meter hast"""
 
 receive_num = int(input("add bede begam chand centimetere ? va chand meter?(!!faghat 4 ragham!!): "))
+
+if 999 < receive_num < 10000:
+    meter = receive_num // 1000
+    centimeter = receive_num // 10
+    print(f"addi ke be onvan milimeter vared cardi: {receive_num}")
+    print(f"meter -> {( meter )} meter")
+    print(f"centimeter -> {( centimeter )} centimeter")
+else:
+    print("4 raghami khastam na kamtar na bishtar!")
