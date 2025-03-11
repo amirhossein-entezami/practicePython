b74fc4abcf2b0b730fa3710c8222f73f7f1639da
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-11 23:56:32 +0330

    show us whats you recommend

--- Project/triangle.py
+++ Project/triangle.py
@@ -2,3 +2,5 @@
 kone aya tashkil mosalas mide ya na? """
 
 receive_user = input("3 add vared konid bebinam taskil mosals mide ya na: ").split()
+print("!!!Attention: havaseton bashe baraye sakht mosalas bayad har 3 zelesh ba ham 180 beshan ya kamtar az 180 beshan!!!")
+
