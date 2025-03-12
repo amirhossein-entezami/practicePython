60762676bff495647c5aa4695862f7105746da75
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-12 23:23:00 +0330

    coding way 2 in abs.py

--- Project/abs.py
+++ Project/abs.py
@@ -3,8 +3,11 @@ receive_user = int(input("ye add vared kon ghdr motlagh bedam: "))
 
 # We have 2 way for caculate abs
 """ Way 1 """
-if receive_user > -99999999999999:
-    ghdr_motlagh = receive_user * -1
-    print(ghdr_motlagh)
-else:
-    print(receive_user)
+# if receive_user > -99999999999999:
+#     ghdr_motlagh = receive_user * -1
+#     print(ghdr_motlagh)
+# else:
+#     print(receive_user)
+
+""" Way 2 """
+print(abs(receive_user))
