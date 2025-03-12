496e5347cad644e30ec7bf1de573cb1489f25cee
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-12 23:17:47 +0330

    make a new file by name even.py in project and type code and program

--- Project/even.py
+++ Project/even.py
@@ -1,1 +1,6 @@
 """ barnamei ke barresi mikone ke yek add 2 raghami hast ya na  """
+recive_user = int(input("Lotfan ye add 2 raghami vared konid:"))
+if 10 <= recive_user <= 99:
+    print("Bale 2 raghami hast!")
+else:
+    print("Na 2 raghami nist!")
