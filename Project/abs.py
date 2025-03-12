1d7a1371ad52052cd017ad1262a155755e5c2866
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-12 23:22:21 +0330

    coding way 1 in abs.py

--- Project/abs.py
+++ Project/abs.py
@@ -1,1 +1,10 @@
 """ Barnamei ke kar tabe ghdr motlagh ra anjam midahad! """
+receive_user = int(input("ye add vared kon ghdr motlagh bedam: "))
+
+# We have 2 way for caculate abs
+""" Way 1 """
+if receive_user > -99999999999999:
+    ghdr_motlagh = receive_user * -1
+    print(ghdr_motlagh)
+else:
+    print(receive_user)
