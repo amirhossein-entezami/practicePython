51c2d47e8fda3f0d99032c251b0b2af46ebbf479
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-13 14:55:06 +0330

    add new file in project directory -> squre.py

--- Project/squre.py
+++ Project/squre.py
@@ -2,3 +2,9 @@
 print("2 add vared konid!")
 num_1 = int(input("add aval: "))
 num_2 = int(input("add dovom: "))
+
+if num_1 == num_2:
+    moraba = (num_2 + num_1 / 2) ** 2 - num_1 ** 2 / 4 
+    print("moraba tashkil mishavad!")
+else:
+    print("moraba nist!")
