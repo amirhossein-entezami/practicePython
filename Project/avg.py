1cf9b02f515f1d75eb2d22f3097d9a624adbb96b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-17 00:01:41 +0330

    add new line for avg.py

--- Project/avg.py
+++ Project/avg.py
@@ -4,3 +4,6 @@ print("3 add ba fasle bezanid miangin bedam")
 num_1 = int(input("enter add 1: "))
 num_2 = int(input("enter add 2: "))
 num_3 = int(input("enter add 3: "))
+
+avg = (num_1 + num_2 + num_3) / 3
+print(f"Miangin = {round(avg)}")
