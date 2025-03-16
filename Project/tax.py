617f33ec02abadb9927379b56f0959ad9fd55d09
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-16 23:52:17 +0330

    add a new line to tax.py

--- Project/tax.py
+++ Project/tax.py
@@ -1,2 +1,4 @@
 """ Barnamei ke gheymat mashion migire age balaye 400t
 bod 20% malyat pash bezane dar gheyr in sorat 10% malyat bezane! """
+receive_user = int(input("Lotfan mablagh morede nazar khod baraye khodro mord nazr benevisid(ex. 300): "))
+
