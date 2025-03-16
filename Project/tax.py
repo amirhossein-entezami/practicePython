2b31c844806104cec99e39f9b94d29643f7ae844
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-16 23:53:10 +0330

    make new condition for tax.py

--- Project/tax.py
+++ Project/tax.py
@@ -1,4 +1,11 @@
 """ Barnamei ke gheymat mashion migire age balaye 400t
 bod 20% malyat pash bezane dar gheyr in sorat 10% malyat bezane! """
 receive_user = int(input("Lotfan mablagh morede nazar khod baraye khodro mord nazr benevisid(ex. 300): "))
-
+if receive_user <= 400:
+    expensive = round(receive_user * 0.2)
+    print(f"Ba ehtesab 20% maliat -> ({expensive}t) bayad bishtar pardakht shavad")
+    print(f"Dar kole bayad -> ({receive_user + expensive}t) pardakht shavad")
+else:
+    cheap = round(receive_user * 0.1)
+    print(f"Ba ehtesab 10% maliat -> ({cheap}t) bayad bishtar pardakht shavad")
+    print(f"Dar kole bayad -> ({receive_user + cheap}t) pardakht shavad")
