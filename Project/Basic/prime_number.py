a3d9d454f0f51d6a957ae842efca03dc0f64e74b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-18 20:42:19 +0330

    add new line in prime_number.py and give receive_user from user

--- Project/Basic/prime_number.py
+++ Project/Basic/prime_number.py
@@ -1,3 +1,4 @@
 """ barnamei ke yek add 3 raghami migire ye add tak ragahmi ham migire be
 onvan meyar baraye sefr kardan khane (n) add aval """
 
+receive_user = int(input("lotfan add 3 raghami vared konid!: "))
