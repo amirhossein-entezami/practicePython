c4741e43fb552e4c95c87588332440f901cda0d6
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-18 20:33:52 +0330

    add if else condition for calculate bmi

--- Project/bmi.py
+++ Project/bmi.py
@@ -4,3 +4,16 @@ vzn salem age bein 24.5 ta 30 bod bege ezafe vazn bishtar az 30 khatr"""
 
 height = int(input("Ghad khod ro vared konid(ex. 178): "))
 weight = int(input("Vazn khod ro vared konid(ex. 90): "))
+
+height = height / 100
+BMI = weight / (height ** 2)
+print(f"Shakhes tode badni shoma-> {BMI}")
+if BMI <= 18:
+    print("shoma kambod vazn darid!")
+elif 18.5 <= BMI <= 24.5:
+    print("shoma vazneton monasebe!")
+elif 24.5 <= BMI <= 30:
+    print("Shoma Ezafe Vazn darid!")
+elif BMI >= 30:
+    print("Warning! You are must be Died!!!!")
+    
