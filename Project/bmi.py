05de1e679e8efe9c5079dd3a5931ba0c7f6e9515
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-18 20:31:42 +0330

    add new file by name bmi.py in project

---
+++ Project/bmi.py
@@ -0,0 +1,4 @@
+""" barnamei benevisid ke ghad va vazn begire shakhes tode badani ro hesab mikone
+age BMI kamtar mosavi 18 bod bege kambod vazn age bein 18.5 ta 24.5 bod bege
+vzn salem age bein 24.5 ta 30 bod bege ezafe vazn bishtar az 30 khatr"""
+
