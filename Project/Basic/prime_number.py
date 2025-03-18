bb8cfdcb4c4d2e682beb734b0414450ff435c371
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-18 20:43:39 +0330

    give receive user from compare in prime_number.py

--- Project/Basic/prime_number.py
+++ Project/Basic/prime_number.py
@@ -2,3 +2,4 @@
 onvan meyar baraye sefr kardan khane (n) add aval """
 
 receive_user = int(input("lotfan add 3 raghami vared konid!: "))
+compare = input("meyar addad bede ke on addo be tartib az chp be rast 0 koanm! 1 - 3: ")
