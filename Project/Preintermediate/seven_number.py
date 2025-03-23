03a70af9e774c0e1feec105a02c92822b43444b7
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-03-23 23:34:06 +0330

    create new variable and new program by name seven_number.py

--- Project/Preintermediate/seven_number.py
+++ Project/Preintermediate/seven_number.py
@@ -1,1 +1,9 @@
 """barname benevisid ke 7 ta add begire hasel jam neshon bde"""
+
+sum = 0
+add = 1
+for i in range(7):
+    num = int(input(f"add {add} vared konid: ", ))
+    sum += num
+    add += 1
+print(f"majmoe addad -> {sum}")
