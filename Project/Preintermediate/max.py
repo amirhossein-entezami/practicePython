344b94f54883cc23641babf41a6c161156a40f8b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-01 18:52:59 +0330

    make roll with code in max.py

--- Project/Preintermediate/max.py
+++ Project/Preintermediate/max.py
@@ -1,2 +1,12 @@
 """ barnamei benvisid 10 add begire bege max kodome """
 print("10 add delkhah vared konid, ta maximum behet bedam!")
+
+add = 1
+max = 0
+for i in range(10):
+    num = int(input(f"number {add}: "))
+    add += 1
+    if num > max:
+        max = num
+print(f"maximum -> {max}")
+
