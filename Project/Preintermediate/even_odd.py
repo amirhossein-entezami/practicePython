a0a401cef821ab993c3429c1ad48979eaad780c9
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-01 18:48:38 +0330

    make roll for even_odd.py

--- Project/Preintermediate/even_odd.py
+++ Project/Preintermediate/even_odd.py
@@ -1,2 +1,12 @@
 """ barname benvisid ke 5 ta add begire bege zoje ya farde """
 print("5 bar add vared konid! ke bebinim zoje ya fard!")
+
+add = 1
+for count in range(5):
+    num = int(input(f"number {add}: "))
+    add += 1
+    if num % 2 == 0:
+        print(f"{num} is -> even")
+    else:
+        print(f"{num} is -> odd")
+
