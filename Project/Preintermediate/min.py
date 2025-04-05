552072d4a769a5fc19d7950a956b2c230f7c71eb
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-05 23:13:27 +0330

    make roll for min.py in preintermediate

--- Project/Preintermediate/min.py
+++ Project/Preintermediate/min.py
@@ -1,3 +1,13 @@
 """ barnamei benvisid 10 add begire bege min kodome """
 
 print("10 add delkhah vared konid, ta minimum behet bedam!")
+
+add = 2
+recive_min = int(input("number 1: "))
+min = recive_min
+for i in range(9):
+    num = int(input(f"number {add}: "))
+    add += 1
+    if num < min:
+        min = num
+print(f"minimum is - > {min}")
