990e5782be1de5ce53cd43385c07a3c75da212d1
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-07 20:46:14 +0330

    make roll for compare.py in preintermediate

--- Project/Preintermediate/compare.py
+++ Project/Preintermediate/compare.py
@@ -1,2 +1,6 @@
 """barnamei benevisid ke yek add begire maghsomaleeyesho beshmare"""
 
+recive = int(input("ye add vared kon maghsom allyehasho behet bedam: "))
+for i in range(1, recive):
+    if recive % i == 0:
+        print(i)
