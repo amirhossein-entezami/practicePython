3f226c3ab0fa77654db6756c0399783823dd1a72
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-07 20:49:58 +0330

    make roll way 1 for squre.py in preintermediate

--- Project/Preintermediate/squre.py
+++ Project/Preintermediate/squre.py
@@ -1,2 +1,14 @@
 """barnamei benevisid ke yek add migire agr morabaee
 kamel bod faktoreilsh ro hesab mikone!"""
+
+
+"""Way 1"""
+
+factorial = int(input("ye add verd konid morabee begiram faktoreyl bedam: "))
+if factorial % 4 == 0 or factorial % 4 == 1:
+    print("in addadi ke dadi morabee kamele!")
+    for i in range(1, factorial):
+        factorial *= i
+    print("factorial in add barabar: ", factorial)
+else:
+    print("moraba nist!")
