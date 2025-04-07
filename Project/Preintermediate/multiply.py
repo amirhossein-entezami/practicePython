0ab5c07d244728c3ab9f4ccb713a6cfd826d1d9e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-07 20:43:29 +0330

    add new roll for multiply.py

--- Project/Preintermediate/multiply.py
+++ Project/Preintermediate/multiply.py
@@ -1,2 +1,10 @@
 """barnamei benevisid yek add va yek zarib daryaft kone va
 amal zarb an add dar zaribesh ro 10 bar tekrar mikone"""
+
+add = int(input("ye add delkhah vared konid: "))
+zarib = int(input("ye add be onvan zarib addet vared kon: "))
+num = 0
+for i in range(10):
+    add *= zarib
+    num += 1
+    print(f"dafee {num}: ", add)
