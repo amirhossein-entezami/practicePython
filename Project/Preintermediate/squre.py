1be58764d554d3162e61e1e0945c4b1ec8e7ed4a
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-07 20:50:54 +0330

    make roll way 2 with math library in squre.py in preintermediate

--- Project/Preintermediate/squre.py
+++ Project/Preintermediate/squre.py
@@ -1,14 +1,29 @@
 """barnamei benevisid ke yek add migire agr morabaee
 kamel bod faktoreilsh ro hesab mikone!"""
+import math
 
 
 """Way 1"""
 
-factorial = int(input("ye add verd konid morabee begiram faktoreyl bedam: "))
-if factorial % 4 == 0 or factorial % 4 == 1:
-    print("in addadi ke dadi morabee kamele!")
-    for i in range(1, factorial):
-        factorial *= i
-    print("factorial in add barabar: ", factorial)
+# factorial = int(input("ye add verd konid morabee begiram faktoreyl bedam: "))
+# if factorial % 4 == 0 or factorial % 4 == 1:
+#     print("in addadi ke dadi morabee kamele!")
+#     for i in range(1, factorial):
+#         factorial *= i
+#     print("factorial in add barabar: ", factorial)
+# else:
+#     print("moraba nist!")
+
+
+"""Way 2"""
+
+
+sq = 1
+num = int(input("enter number: "))
+
+if math.sqrt(num) == int(math.sqrt(num)):
+    for z in range(1,num + 1):
+        sq *= z
+    print("factorail:" , sq)
 else:
-    print("moraba nist!")
+    print("number is not square!")
