074513529e8b1354cc1e2703b62e3df09a4b77c9
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-08 21:56:13 +0330

    make roll in fibonanchi.py

--- Project/Preintermediate/fibonanchi.py
+++ Project/Preintermediate/fibonanchi.py
@@ -1,3 +1,12 @@
 """ fibonanchi """
-
+num_1 = 1
+num_2 = 1
+num_3 = 0
+my_counter = 0
 counter = int(input("addad vared konid: "))
+while my_counter <= counter:
+    print(f"{num_3}")
+    num_1, num_2 = num_2, num_3
+    # num_2 = num_3
+    num_3 = num_1 + num_2
+    my_counter += 1
