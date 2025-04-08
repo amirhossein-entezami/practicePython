594b51ff146ff83b1352684d92bccbe047a2e46b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-08 21:53:38 +0330

    make roll for perfect_number.py in preintermediate

--- Project/Preintermediate/perfect_number.py
+++ Project/Preintermediate/perfect_number.py
@@ -2,3 +2,13 @@
 add kamele ya na age kamel bod payam bede perfect"""
 
 number = int(input("ye add vared kon ke begam kamele ya na! : "))
+
+sum = 0
+for i in range(1, number):
+    if number % i == 0:
+        sum += i
+if sum == number:
+    print("perfect!")
+else:
+    print("perfect nist!")
+
