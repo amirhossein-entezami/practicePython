4d5060a80af64117850b1b8493c911f4cb3b31a9
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-09 22:59:53 +0330

    make roll for reverse.py in intermediate

--- Project/intermediate/reverse.py
+++ Project/intermediate/reverse.py
@@ -2,3 +2,14 @@
 va barresi kone on add ba maghlobesh barabare ya na ex. 522 -> 225 (*) - 303 -> 303 (+) """
 
 receive_user = input("please enter a number: ")
+
+if int(receive_user) == 10:
+    print("End!")
+else:
+    reverse_user = receive_user[::-1]
+    print(f"your input number is -> {receive_user}")
+    print(f"Reverse your input is -> {reverse_user}")
+    if int(receive_user) == int(reverse_user):
+        print("your final result is -> **yes!**")
+    else:
+        print("your final result is -> --no!--")
