0fee2dc4390c6da8c9e9ff07c56eaaeb0c29de78
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-09 23:03:29 +0330

    make roll for palinrom.py in intermediate

--- Project/intermediate/palinrom.py
+++ Project/intermediate/palinrom.py
@@ -2,3 +2,15 @@
 va barrresi mikone palinrom barabare ya na!"""
 
 recevie_char = input("please eneter name: ")
+if int(recevie_char) % 2 == 0:
+    print("this is integer az even!")
+else:
+    while True:
+        recevie_char_inverse = recevie_char[::-1]
+        if recevie_char == recevie_char_inverse:
+            print("Yes this name is inverse!")
+            print(f"inverse: {recevie_char_inverse}")
+        else:
+            print("No it's not inverse")
+            print(f"inverse: {recevie_char_inverse}")
+        recevie_char = input("please eneter name: ")
