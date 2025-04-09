2dc7fa609b0e1f33df41861c17b66d0873b2f69e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-09 22:59:19 +0330

    receive user in reverse.py in intermediate

--- Project/intermediate/reverse.py
+++ Project/intermediate/reverse.py
@@ -1,2 +1,4 @@
 """ barnamei benvisid ke ta zamani ke user add 10 nanade az karbar add begire
 va barresi kone on add ba maghlobesh barabare ya na ex. 522 -> 225 (*) - 303 -> 303 (+) """
+
+receive_user = input("please enter a number: ")
