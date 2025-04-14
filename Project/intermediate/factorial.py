9f2214dea3972dbf47137899eddbb5b39f509cf9
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-14 22:48:56 +0330

    make roll from factorial.py in intermediate

--- Project/intermediate/factorial.py
+++ Project/intermediate/factorial.py
@@ -1,3 +1,8 @@
 """barnamei ke factorial add ro namayesh bede"""
 
 receive_user = int(input("please enter a number: "))
+
+factorail = 1
+for i in range(1, receive_user+1):
+    factorail = factorail * i
+print(f"the factorial of {receive_user} is {factorail}")
