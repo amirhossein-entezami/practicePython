9486465bc8e88cebf71c488650acef72fe999228
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-14 22:46:46 +0330

    make roll from division.py in intermediate

--- Project/intermediate/division.py
+++ Project/intermediate/division.py
@@ -1,3 +1,6 @@
 """ barnamei ke yek add migire tamam maghsom alayeh hasho chap mikone khode addo negah midare! """
 
 receive_user = int(input("Please Enter a number: "))
+for shomaresh in range(1, receive_user):
+    if receive_user % shomaresh == 0:
+        print(shomaresh)
