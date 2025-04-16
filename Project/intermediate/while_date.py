109869c9ba7785d7bed8dda8eab2dc82ecd80fec
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-16 21:43:24 +0330

    add new line by name while_date.py in intermediate

---
+++ Project/intermediate/while_date.py
@@ -0,0 +1,4 @@
+""" barnamei ke ta harbar 10 nadade esm famil tarikh tavalod jensiat daryaft karde
+va eltelat ra joda joda namayesh midahad khat aval etelat nafar aval
+khatdovom etelat nafar dovom"""
+
