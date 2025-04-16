c4ce6ef225ec4dafcd4368cfd2f4861f7d533558
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-16 21:35:55 +0330

    make roll for prime.py

--- Project/intermediate/prime.py
+++ Project/intermediate/prime.py
@@ -1,3 +1,12 @@
 """ barnamei ke yek add mikhone mige avale ya na  """
 
 receive_user = int(input("please enter a number: "))
+
+counter = 0
+for aval in range(1, receive_user+1):
+    if receive_user % aval == 0:
+        counter += 1
+if counter == 2:
+    print("bale add avale!")
+else:
+    print("kheyr add aval nist!")
