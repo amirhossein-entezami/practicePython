2c7899085275ad164a6d68fba85bc0d26f59e9a9
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-16 21:45:47 +0330

    new role for while_date.py

--- Project/intermediate/while_date.py
+++ Project/intermediate/while_date.py
@@ -4,3 +4,9 @@ khatdovom etelat nafar dovom"""
 
 receive_user_a = int(input("please enter a sen: "))
 
+while receive_user_a != 10:
+    receive_user_n = input("please enter a name: ")
+    receive_user_f = input("please enter a family: ")
+    receive_user_j = input("please enter a jensiat: ")
+    print(f"{receive_user_n} , {receive_user_f}, {receive_user_a}, {receive_user_j}")
+    
