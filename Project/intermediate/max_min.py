606f5f7b1642445cec029f9f276a5665fc2d6de1
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-19 17:49:32 +0330

    create new file by name max_min.py in intermediate and receive from user

--- Project/intermediate/max_min.py
+++ Project/intermediate/max_min.py
@@ -1,2 +1,5 @@
 """ barnamei ke ta vaghti ke karbar 0 nadadeh add bgire dar nahayat
 majome tedad miangin max va min ro elam kone"""
+
+receive_user = int(input("please enter a number: "))
+receive_user_2 = int(input("please enter another number: "))
