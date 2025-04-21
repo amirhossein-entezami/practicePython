97e419920519f5180aea19e9ce38670117de0e35
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-21 23:07:58 +0330

    receive from user in str_row.py in Advanced

--- Project/Advanced/str_row.py
+++ Project/Advanced/str_row.py
@@ -1,2 +1,4 @@
 """ 1- barnamei ke yek reshte ra khandeh va har kalame ra dar kenar code ascii an zakhire karde va darnayt
 tamami an ra namayesh midahad va hamchenin migoyad kodamin kalame bozorgtrain code assci ra dashte"""
+
+receive_user = input("plz enter word: ")
