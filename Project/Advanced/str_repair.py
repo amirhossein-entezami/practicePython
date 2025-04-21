e15ee75cb6c3f43f579a3d16f587cf6d1f8c702e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-21 23:12:39 +0330

    add new role for str_repair.py

--- Project/Advanced/str_repair.py
+++ Project/Advanced/str_repair.py
@@ -2,3 +2,16 @@
 va tedad tekrar haye kalamat ra be hamrah khode an namayesh bede """
 
 receive_user = input("please enter name of sport: ")
+
+tedad = 0
+ind_ = 0
+repeat = ""
+while ind_ < len(receive_user):
+    shomrandeh = 0
+    for num_reap in receive_user:
+        if num_reap == receive_user[ind_]:
+            shomrandeh += 1
+    if receive_user[ind_] not in repeat:
+        print(receive_user[ind_], "=", shomrandeh)
+        repeat += receive_user[ind_]
+    ind_ += 1
