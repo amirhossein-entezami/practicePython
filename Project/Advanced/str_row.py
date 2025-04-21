3e6233f2e64399eb0164058ff38235d037d9d308
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-21 23:08:32 +0330

    add new role for str_row.py

--- Project/Advanced/str_row.py
+++ Project/Advanced/str_row.py
@@ -2,3 +2,13 @@
 tamami an ra namayesh midahad va hamchenin migoyad kodamin kalame bozorgtrain code assci ra dashte"""
 
 receive_user = input("plz enter word: ")
+
+row = ""
+max = ord(receive_user[0])
+for repeat in receive_user:
+    ord_ = ord(repeat)
+    print(f"{repeat}  ->  {ord_}")
+    if ord_ > max:
+        chr_= chr(ord_)
+        max = ord_
+print(f"bishtarin meghdar kalame-> ( {chr_} ) ke ascci barabr -> ( {max} )")
