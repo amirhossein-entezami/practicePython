5c6318a0e23726dbb061a8a97dc5ebff9577f5bf
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-21 23:11:57 +0330

    receive user form str_repair.py

--- Project/Advanced/str_repair.py
+++ Project/Advanced/str_repair.py
@@ -1,2 +1,4 @@
 """2- barnamei benevisid ke yek reshte varzeshi daryaft kone 
 va tedad tekrar haye kalamat ra be hamrah khode an namayesh bede """
+
+receive_user = input("please enter name of sport: ")
