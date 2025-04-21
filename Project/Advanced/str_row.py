bbaf4b96e6e5ec22875d284845fdc245a34ed771
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-21 23:06:06 +0330

    create new file str_row.py in Advanced project

---
+++ Project/Advanced/str_row.py
@@ -0,0 +1,2 @@
+""" 1- barnamei ke yek reshte ra khandeh va har kalame ra dar kenar code ascii an zakhire karde va darnayt
+tamami an ra namayesh midahad va hamchenin migoyad kodamin kalame bozorgtrain code assci ra dashte"""
