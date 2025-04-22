5054d46d2159888c98f8d789efdaba5b72e421bd
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-22 18:21:42 +0330

    make role for pass_text.py

--- Project/Advanced/pass_text.py
+++ Project/Advanced/pass_text.py
@@ -1,3 +1,9 @@
 """ barnamei benevisid ke yek matn ra be onvan ramz daryaft kone m an ra be ramz sezar tabdil konad"""
 
 receive_user = input("please enter a word:")
+
+list = ""
+for sezar in receive_user:
+    tak = chr(ord(sezar) + 3)
+    list += tak
+print(list)
