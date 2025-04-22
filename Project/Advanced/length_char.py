1b56960f8c577eda24daf788428b1ed3fae5c6e5
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-22 18:25:20 +0330

    make role with way 2 in length_char.py in Advanced

--- Project/Advanced/length_char.py
+++ Project/Advanced/length_char.py
@@ -4,11 +4,21 @@ receive_user = input("please enter a country: ")
 receive_tekrar = int(input("chnd bakhsh chnd bakkhsh shavad: "))
 
 """ way 1 """
-ezafe = 0
-list = ""
-for joda in receive_user:
-    khoroji = receive_user[ezafe:receive_tekrar]
-    ezafe += 1
-    receive_tekrar += 1
-    print(khoroji)
+# ezafe = 0
+# list = ""
+# for joda in receive_user:
+#     khoroji = receive_user[ezafe:receive_tekrar]
+#     ezafe += 1
+#     receive_tekrar += 1
+    # print(khoroji)
 
+""" way 2 """
+final_word = ""
+for i in range (len(receive_user)):
+    harf = receive_user[i:receive_tekrar + i]
+    if len(harf) < receive_tekrar:
+        break
+    else:
+        # print(receive_user[i:receive_tekrar + i])
+        final_word += harf + " "
+print(final_word)
