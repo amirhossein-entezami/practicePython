5811945f1aa23b3a6ae0754abde8468bf0ac0a0e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-22 18:24:36 +0330

    make role with way 1  in length_char.py in Advanced

--- Project/Advanced/length_char.py
+++ Project/Advanced/length_char.py
@@ -2,3 +2,13 @@
 
 receive_user = input("please enter a country: ")
 receive_tekrar = int(input("chnd bakhsh chnd bakkhsh shavad: "))
+
+""" way 1 """
+ezafe = 0
+list = ""
+for joda in receive_user:
+    khoroji = receive_user[ezafe:receive_tekrar]
+    ezafe += 1
+    receive_tekrar += 1
+    print(khoroji)
+
