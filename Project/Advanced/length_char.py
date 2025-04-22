6ba13202d0be64bfa898c7566b9056ca460b9139
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-22 18:23:55 +0330

    receive from user in length_char.py in Advanced

--- Project/Advanced/length_char.py
+++ Project/Advanced/length_char.py
@@ -1,2 +1,4 @@
 """ barnamei ke yek reshte ra az vorodi be hamrah yek add khandeh, tamai kalamat reshte be tol an add namayesh midahad """
 
+receive_user = input("please enter a country: ")
+receive_tekrar = int(input("chnd bakhsh chnd bakkhsh shavad: "))
