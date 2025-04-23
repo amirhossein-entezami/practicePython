4fe051b26515a0cdcebdbca64d2e19fdefdc15b7
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-23 20:35:44 +0330

    add role for first_test.py

--- Project_techinical_tehran/test/first_test.py
+++ Project_techinical_tehran/test/first_test.py
@@ -1,3 +1,7 @@
 # barnamei benevisid ke mablagh bigire 20% maliat ezafe koneh
 
 receive_user = int(input("Enter your money: "))
+
+tax = int(receive_user * .2)
+all = int(tax + receive_user)
+print(f"Your money is-> {receive_user} and your money with tax is -> {tax} and all of them is-> {all}")
