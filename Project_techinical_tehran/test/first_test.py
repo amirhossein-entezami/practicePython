f9c6ab2a64a03d0d6ec5af91ccc2f54efc5c8ac3
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-25 23:56:41 +0330

    check the file

--- Project_techinical_tehran/test/first_test.py
+++ Project_techinical_tehran/test/first_test.py
@@ -5,3 +5,4 @@ receive_user = int(input("Enter your money: "))
 tax = int(receive_user * .2)
 all = int(tax + receive_user)
 print(f"Your money is-> {receive_user} and your money with tax is -> {tax} and all of them is-> {all}")
+# check
