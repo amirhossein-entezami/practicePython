71fc2b821ec807e2d4f2366f6ae79c5b8ccdb83b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-26 21:33:04 +0330

    add new role and import math in second_test.py

--- Project_techinical_tehran/test/second_test.py
+++ Project_techinical_tehran/test/second_test.py
@@ -1,1 +1,6 @@
 # barnamei benevisid key ye add bigire jazresho namayesh bede
+
+import math
+receive_user = int(input("Enter a number for sqrt: "))
+square_root = int(math.sqrt(receive_user))
+print(f"jazre addet-> {square_root}")
