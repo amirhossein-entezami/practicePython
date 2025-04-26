bf76ec2b77451b0549fd80f950e0b5aee6fd9dd0
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-26 21:36:41 +0330

    add new line and role in third_test.py

--- Project_techinical_tehran/test/third_test.py
+++ Project_techinical_tehran/test/third_test.py
@@ -1,1 +1,4 @@
 # barnamei benvisid ke yek add begire be milimeter bad bege on add chnd metre vo chnd santimetro o chand milimetere?
+
+receive_user = int(input("Enter your number as milimeter: "))
+
