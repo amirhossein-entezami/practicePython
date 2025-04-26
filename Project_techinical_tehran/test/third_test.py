1c682700a301a99c13b063031b834f028aa4c949
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-26 21:37:09 +0330

    work with role

--- Project_techinical_tehran/test/third_test.py
+++ Project_techinical_tehran/test/third_test.py
@@ -2,3 +2,8 @@
 
 receive_user = int(input("Enter your number as milimeter: "))
 
+# receive user is milimeter
+meter = int(receive_user / 1000)
+centimeter = int((receive_user - (meter * 1000)) / 10)
+milimeter = int(receive_user - ((meter * 1000) + (centimeter * 10)))
+print(f"{meter}-> meter, {centimeter} -> centimeter , {milimeter}-> milimeter")
