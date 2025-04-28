45d1502970c76519e0cf067f6a326c2cfb6fc086
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-28 16:13:03 +0330

    add new role for fifth_test.py

--- Project_techinical_tehran/test/fifth_test.py
+++ Project_techinical_tehran/test/fifth_test.py
@@ -1,3 +1,10 @@
 # barnamei ke add ro barresi kone bebine 2 raghamie ya na
 
 receive_user = input("Enter your Number: ")
+
+if len(receive_user) < 2:
+    print("Kamtar az 2 ragham")
+elif len(receive_user) > 2:
+    print("bisar az 2 ragham")
+elif len(receive_user) == 2:
+    print("2 raghami hast")
