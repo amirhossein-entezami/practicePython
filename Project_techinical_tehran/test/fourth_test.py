337a4a988aee2c22e75fd63cd19d344840b27e46
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-28 16:09:19 +0330

    add if condition for fourth_test.py

--- Project_techinical_tehran/test/fourth_test.py
+++ Project_techinical_tehran/test/fourth_test.py
@@ -5,3 +5,14 @@ receive_user = input("enter 3 numbers: ").split()
 x = int(receive_user[0])
 y = int(receive_user[1])
 z = int(receive_user[2])
+
+if 10 < x < 60:
+    if 10 < y < 60:
+        if 10 < z < 60:
+            print("yes make a triangle! ")
+        else:
+            print("no, do not make a triangle!")
+    else:
+        print("no, do not make a triangle!")
+else:
+    print("no, do not make a triangle!")
