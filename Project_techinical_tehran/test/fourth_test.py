242cea1d37b36af480add160c281b1fe17543e8a
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-04-28 16:08:13 +0330

    add new role for fourth_test.py

--- Project_techinical_tehran/test/fourth_test.py
+++ Project_techinical_tehran/test/fourth_test.py
@@ -1,3 +1,7 @@
 # barnamei benvisid ke se ta add begire bege tashkil mosalas midan ya kheir
 
 receive_user = input("enter 3 numbers: ").split()
+
+x = int(receive_user[0])
+y = int(receive_user[1])
+z = int(receive_user[2])
