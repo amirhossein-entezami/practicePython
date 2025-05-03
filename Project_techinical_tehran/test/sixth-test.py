eee74c34d70989e8b64e2ed005eab1c09d8634b1
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-03 18:59:00 +0330

    add new role with way 2

--- Project_techinical_tehran/test/sixth-test.py
+++ Project_techinical_tehran/test/sixth-test.py
@@ -3,5 +3,14 @@
 receive_user = int(input("Enter number: "))
 
 # first way is->
-abs_receive_user = abs(receive_user)
-print(f"ghadr motlagh addet-> {abs_receive_user}")
+# abs_receive_user = abs(receive_user)
+# print(f"ghadr motlagh addet-> {abs_receive_user}")
+
+
+# second way is ->
+if receive_user > 0:
+    print(f"ghadr motlagh addet-> {receive_user}")
+elif receive_user < 0:
+    print(f"ghadr motlagh addet-> {receive_user * -1}")
+elif receive_user == 0:
+    print(f"sefr mosbate dar natije khodeshe-> {receive_user}")
