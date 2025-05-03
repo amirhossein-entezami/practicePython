d50a7631f2b4ac9bdad446107090f2067d3266fe
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-03 18:58:17 +0330

    add new role way 1

--- Project_techinical_tehran/test/sixth-test.py
+++ Project_techinical_tehran/test/sixth-test.py
@@ -1,3 +1,7 @@
 # barnamei ke tabe ghadr motlagh anjam mideh
 
 receive_user = int(input("Enter number: "))
+
+# first way is->
+abs_receive_user = abs(receive_user)
+print(f"ghadr motlagh addet-> {abs_receive_user}")
