fe0220840ee184ac4548c92a4d19b80c8fb9bff2
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-03 19:22:13 +0330

    add new role in seventh_test

--- Project_techinical_tehran/test/seventh_test.py
+++ Project_techinical_tehran/test/seventh_test.py
@@ -2,3 +2,8 @@
 receive_user_a = int(input("Enter your first zele: "))
 receive_user_b = int(input("Enter your second zele: "))
 
+if receive_user_a == receive_user_b:
+    moraba = ((receive_user_a + receive_user_b) / 2) ** 2 - receive_user_a ** 2 / 4
+    print(f"bale tashkil mishavad va add nahaii ine -> {moraba}")
+else:
+    print("na tashkil nemishe! ")
