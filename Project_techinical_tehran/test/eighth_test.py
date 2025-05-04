bc7ffd8894ed37713059aea1a143e9e89a8754b0
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-04 20:08:03 +0330

    add new role for eighth_test.py

--- Project_techinical_tehran/test/eighth_test.py
+++ Project_techinical_tehran/test/eighth_test.py
@@ -1,3 +1,12 @@
 # barnamei benevisid ke gheymat mashid daryaft card age balaye 400m bashe 20% ezafe koneh dar gheyr insorat 10% ezafe koneh.
 
 receive_user = int(input("Enter your price for car: "))
+
+count = 400
+if receive_user <= count:
+    tax = int(receive_user * .2)
+    print(f"Your car price is ${receive_user} and your tax is ${tax} and your payment is ${receive_user + tax}")
+elif receive_user >= count:
+    tax = int(receive_user * .1)
+    print(f"Your car price is ${receive_user} and your tax is ${tax} and your payment is ${receive_user + tax}")
+    
