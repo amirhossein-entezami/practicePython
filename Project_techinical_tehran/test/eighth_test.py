deb6436b520137e1359e917eb8d2e387d4c22c2b
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-04 20:07:22 +0330

    receive from user

--- Project_techinical_tehran/test/eighth_test.py
+++ Project_techinical_tehran/test/eighth_test.py
@@ -1,1 +1,3 @@
 # barnamei benevisid ke gheymat mashid daryaft card age balaye 400m bashe 20% ezafe koneh dar gheyr insorat 10% ezafe koneh.
+
+receive_user = int(input("Enter your price for car: "))
