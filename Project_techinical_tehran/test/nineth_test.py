d301d5488557f590106ff7e46e63dc498063e4b5
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-04 20:11:26 +0330

    add new role for nineth_test.py

--- Project_techinical_tehran/test/nineth_test.py
+++ Project_techinical_tehran/test/nineth_test.py
@@ -1,3 +1,14 @@
 # barnamei benevisid ke 3 ta add begire avg ro bede behomon
 
 receive_user = input("3 ta add bede (ba fasele benvis) ta avg ro bedam: ").split()
+
+x = int(receive_user[0])
+y = int(receive_user[1])
+z = int(receive_user[2])
+count = 0
+for i in receive_user:
+    count += 1
+
+avg = (x + y + z) / count
+print(f"avarage: {avg}")
+
