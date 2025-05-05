59a61d1cbffa7aff36e5cc5eae37b77e26ac583e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-05 19:15:14 +0330

    create menu with print to show user what he/she can choose!

--- Project_techinical_tehran/CRUD/main.py
+++ Project_techinical_tehran/CRUD/main.py
@@ -1,3 +1,11 @@
 # CRUD -> Crate Read Update Delete
 
 standard_list = []
+
+print("\n👍->  please choose")
+print("1) Add Student") # record yek danesh amoz ro daryaft kone va zakhire kone!
+print("2) Delete Student") # namayesh tamam danesh amoz ha va entekhab baraye hazf!
+print("3) Replace Student") # kolesh namayesh dade beshe - yek record entekhab bashe va maghadir jadid beshe midim!
+print("4) Show Information") # neshon dadan etelwat - miad behet shomare desnshjo mide!    
+print("5) Find Student") # neshon dadan etelwat - miad behet shomare desnshjo mide!
+print("6) Exit\n") # khoroj az barnameh
