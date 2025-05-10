23ab0f6b49a09f01b822e6aa6a81fcd3d3ccd1b7
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-10 20:50:22 +0330

    check file because we have bug in turtle.py

--- project_100_days_python/GUI/Turtle/Faz_1/turtle.py
+++ project_100_days_python/GUI/Turtle/Faz_1/turtle.py
@@ -64,12 +64,12 @@ timmy.pensize(15)
 timmy.speed("fastest")
 
 # Function to generate a random color
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r, g, b)  # Fixed typo
-    return random_color
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r, g, b)  # Fixed typo
+#     return random_color
 
 # Random walk logic
 # directions = [0, 90, 180, 270]
