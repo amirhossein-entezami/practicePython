464f42b67ade656f5b731215735e5a8164552abc
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-12 18:10:14 +0330

    start and end of turtle in main.py in snake_board directory

--- project_100_days_python/GUI/Turtle/snake_borad/main.py
+++ project_100_days_python/GUI/Turtle/snake_borad/main.py
@@ -3,3 +3,11 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+screen.exitonclick()
