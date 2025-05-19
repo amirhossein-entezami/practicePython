076a61923695b42ebc57ecac00c73a8bf4b9c37e
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-05-19 21:17:03 +0330

    add new variable for screen

--- project_100_days_python/GUI/PingPong/main.py
+++ project_100_days_python/GUI/PingPong/main.py
@@ -4,4 +4,8 @@ from ball import Ball
 from scoreboard import Coreboard
 import time
 
-
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
