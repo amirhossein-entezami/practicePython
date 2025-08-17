489032d2caa9438d5c58eb065fb125439521dfb2
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-08-17 23:07:00 +0330

    add new test.txt with mode

--- project_100_days_python/Learning/open_write_read/main.py
+++ project_100_days_python/Learning/open_write_read/main.py
@@ -1,4 +1,8 @@
-with open("test.txt") as file:
-    contents = file.read()
-    print(contents)
-    file.close()
+# with open("test.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#     file.close()
+    
+    
+with open("test.txt", mode="a") as file: # a -> append , w -> write
+    file.write("\nnew text.")
