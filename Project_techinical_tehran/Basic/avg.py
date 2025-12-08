43c7ba865d2924f9a8d4624700098896cb7236ec
Amirhosein Entezami <am.entezami1321@gmail.com>
2025-12-08 19:55:04 +0330

    test 1

--- Project_techinical_tehran/Basic/avg.py
+++ Project_techinical_tehran/Basic/avg.py
@@ -6,4 +6,5 @@ num_2 = int(input("enter add 2: "))
 num_3 = int(input("enter add 3: "))
 
 avg = (num_1 + num_2 + num_3) / 3
-print(f"Miangin = {round(avg)}")
+print(f"Miangin = {round(avg)}")3 
+#test 8 7 6 5 4 3 2 1
